16a975869851d63e4a6183ce81e9b83eba769693
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-08-26 00:33:49 +0530

    Folder for footer player sidebaroption

--- src/Player.js
+++ src/Player.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Player.css";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
 
-function 
-Player({spotify}) {
-    return (
-        <div>
-            <h2>Welcome to rporfile</h2>
-        </div>
-    )
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body />
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default 
-Player
+export default Player;
